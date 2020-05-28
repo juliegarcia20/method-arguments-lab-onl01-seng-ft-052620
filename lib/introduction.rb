f28a8711960ee698d0a9ef5a34a3888d5b580605
Julie Garcia <juliegarcia.cali@gmail.com>
2020-05-28 01:59:42 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,5 +3,8 @@
 def introduction(name)
   puts "Hi, my name is #{name}."
 end
+def introduction_with_language(name="Ruby Programmer", language="Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
 
